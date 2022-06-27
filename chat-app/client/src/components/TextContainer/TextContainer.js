e701c71067f09cb99907b036c1232d895dc32cf0
veerprakash28 <veer.prakash_cs.aiml19@gla.ac.in>
2022-06-27 17:31:29 +0530

    Adding Chat-Application

---
+++ chat-app/client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import onlineIcon from "../../icons/onlineIcon.png";
+
+import "./TextContainer.css";
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    {users ? (
+      <div>
+        <div>
+          <h2>
+            Welcome to the Chat App{" "}
+            <span role="img" aria-label="emoji">
+              💬
+            </span>
+          </h2>
+          <p>
+            Chat with your friends online{" "}
+            <span role="img" aria-label="emoji">
+              😀
+            </span>
+          </p>
+          <p className="bottom">Share the link and the RoomCode with them</p>
+        </div>
+
+        <h2 className="peopleOnline">People Online:</h2>
+        <div className="activeContainer">
+          <h3>
+            {users.map(({ name }) => (
+              <div key={name} className="activeItem">
+                <img src={onlineIcon} alt="Online Icon" />
+                {name}
+              </div>
+            ))}
+          </h3>
+        </div>
+      </div>
+    ) : null}
+  </div>
+);
+
+export default TextContainer;
