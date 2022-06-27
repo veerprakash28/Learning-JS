f3f8f8f0ea05218b3074d800ba2d0a51b1d6660f
veerprakash28 <veer.prakash_cs.aiml19@gla.ac.in>
2022-06-28 00:34:04 +0530

    final commit

--- chat-app/client/src/components/Chat/Chat.js
+++ chat-app/client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "https://learning-js-chat-app.herokuapp.com/";
+  const ENDPOINT = "https://learning-js-chat-application.herokuapp.com/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
