575031f1b15cffc7caff3934fcf624fcb4db52c5
veerprakash28 <veer.prakash_cs.aiml19@gla.ac.in>
2022-07-26 01:27:30 +0530

    changes

--- Light-Bulb/main.js
+++ Light-Bulb/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let bulb = document.getElementById("bulb");
 let btn = document.getElementById("toggleBtn");
 
@@ -13,19 +12,3 @@ function toggleBulb(e) {
     btn.textContent = "Turn On";
   }
 }
-=======
-let bulb = document.getElementById("bulb");
-let btn = document.getElementById("toggleBtn");
-
-btn.addEventListener("click", toggleBulb);
-
-function toggleBulb(e) {
-  if (btn.textContent.includes("On")) {
-    bulb.src = "img/bulb-on.jpg";
-    btn.textContent = "Turn Off";
-  } else {
-    bulb.src = "img/bulb-off.jpg";
-    btn.textContent = "Turn On";
-  }
-}
->>>>>>> main
