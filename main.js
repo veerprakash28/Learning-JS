b715ebee3a01c2fbf4db9a8d851b1a7190bb5edb
veerprakash28 <veer.prakash_cs.aiml19@gla.ac.in>
2023-06-26 16:50:38 +0530

    code refractor

---
+++ main.js
@@ -0,0 +1,27 @@
+import { projects } from "./projectsList.js";
+const outerContainer = document.querySelector(".outer-container");
+
+const projectCards = projects.map((project) => {
+  let demoButton = "";
+  let codeButton = "";
+
+  project.demoLink
+    ? (demoButton = `<a href="${project.demoLink}" class="btn btn-success" target="_blank">See Demo</a>`)
+    : "";
+
+  project.codeLink
+    ? (codeButton = `<a href="${project.codeLink}" class="btn btn-warning" target="_blank">See Code</a>`)
+    : "";
+
+  return `<div class="card inner-container" style="width: 18rem">
+      <img class="card-img-top" src="${project.image}" alt="${project.title} Image" />
+      <div class="card-body">
+        <h5 class="card-title">${project.title}</h5>
+        <p class="card-text">${project.description}</p>
+        ${demoButton}
+        ${codeButton}
+      </div>
+    </div>`;
+});
+
+outerContainer.innerHTML = projectCards.join("");
---
+++ projectsList.js
@@ -0,0 +1,74 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Analog Clock",
+    image: "./assets/images/analog-clock.png",
+    description:
+      "Watch time unfold with precision and style on this interactive analog clock created using the perfect blend of JavaScript, HTML, and CSS.",
+    demoLink: "Analog-Clock/",
+    codeLink: "",
+  },
+  {
+    id: 2,
+    title: "Calculator App",
+    image: "./assets/images/calculator-app.png",
+    description:
+      "Simplify calculations with our stylish HTML, CSS & JavaScript calculator app. Stylish & functional for all your mathematical needs.",
+    demoLink: "calculator-app/",
+    codeLink: "",
+  },
+  {
+    id: 3,
+    title: "To-Do List",
+    image: "./assets/images/todo-list.png",
+    description:
+      "Stay organized and boost productivity with this sleek to-do list application, seamlessly blending HTML, CSS, Bootstrap, & JavaScript.",
+    demoLink: "To-Do-List/",
+    codeLink: "",
+  },
+  {
+    id: 4,
+    title: "Real-Time Chat App",
+    image: "./assets/images/chat-app.png",
+    description:
+      "Experience the power of real-time communication with this chat app built using JavaScript, React, Socket.io, and Node.js.",
+    demoLink: "https://bit.ly/3yqGqod",
+    codeLink: "",
+  },
+  {
+    id: 5,
+    title: "Your Blog Site",
+    image: "./assets/images/blogsite.png",
+    description:
+      "Explore a dynamic and immersive blogging experience brought to life with HTML, CSS, JavaScript, Node.js, and MongoDB.",
+    demoLink: "https://bit.ly/3oDXHaM",
+    codeLink: "",
+  },
+  {
+    id: 6,
+    title: "Contact Book App",
+    image: "./assets/images/contactbook.png",
+    description:
+      "Effortlessly manage your contacts with a feature-rich web application, powered by JavaScript, Node.js, and a reliable SQL database",
+    demoLink: "",
+    codeLink: "https://github.com/veerprakash28/contact-book-app",
+  },
+  {
+    id: 7,
+    title: "Movie Insights App",
+    image: "./assets/images/movie-insights.png",
+    description:
+      "Uncover movies effortlessly with ReactJS. It offers a seamless search experience using functional components, props, hooks, JSX, etc.",
+    demoLink: "https://react-movie-insights-app.netlify.app/",
+    codeLink: "https://github.com/veerprakash28/react-movie-insights-app",
+  },
+  {
+    id: 8,
+    title: "Talk GPT",
+    image: "./assets/images/talk-gpt.png",
+    description:
+      "Utilizing HTML, CSS, JS, Node.js, and OpenAI API, it enables seamless speech and text communication with ChatGPT.",
+    demoLink: "https://veerprakash28.github.io/talk-gpt/",
+    codeLink: "https://github.com/veerprakash28/talk-gpt",
+  },
+];
