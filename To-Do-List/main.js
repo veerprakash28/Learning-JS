bd391128e8714729129399efcbff9de0d15c7a38
veerprakash28 <veer.prakash_cs.aiml19@gla.ac.in>
2023-07-22 23:35:43 +0530

    modifications in todo-list

--- To-Do-List/main.js
+++ To-Do-List/main.js
@@ -9,41 +9,51 @@ function retrieveFromLocalStorage() {
     parentList.innerHTML = localStorage.getItem("taskList");
   }
 }
+
+// ////////////////////////////////////////////
+
+// Input
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("add_btn");
+
+// Add event listener to the input element
+taskInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addTaskFromInput();
+  }
+});
+
+// Add event listener to the Add button
+addBtn.addEventListener("click", addTaskFromInput);
+
+let parentList = document.getElementById("parentList");
+
 // Call the retrieve function when the page loads
 document.addEventListener("DOMContentLoaded", retrieveFromLocalStorage);
 
-let addBtn = document.getElementById("add_btn");
-addBtn.addEventListener("click", addTask);
-let parentList = document.getElementById("parentList");
+// Function to add task using the input value
+function addTaskFromInput() {
+  let currentTask = taskInput.value.trim();
 
-//   console.log(parentList.children[0].className);
-function addTask(e) {
-  if (
-    parentList.children[0].className == "d-flex justify-content-center emptyMsg"
-  ) {
-    parentList.children[0].remove();
+  if (currentTask !== "") {
+    let newLi = document.createElement("li");
+    newLi.className = "list-group-item d-flex justify-content-between";
+    newLi.innerHTML = `<button class="btn btn-success mx-1 btn-sm" onclick="checkTask(this)">
+        <span class="material-symbols-outlined">Check</span>
+        </button>
+        <h4 class="flex-grow-1">${currentTask}</h4>
+        <button class="btn btn-warning mx-1" onclick="editTask(this)">
+          <span class="material-symbols-outlined"> edit </span>
+        </button>
+        <button class="btn btn-danger" onclick="removeTask(this)">
+          <span class="material-symbols-outlined"> delete </span>
+        </button>`;
+    parentList.insertBefore(newLi, parentList.firstChild);
+
+    taskInput.value = "";
+    saveToLocalStorage();
   }
-  let currentBtn = e.currentTarget;
-  let currentInput = currentBtn.previousElementSibling;
-  let currentTask = currentInput.value;
-
-  let newLi = document.createElement("li");
-  // newLi.classList.add("list-group-item");
-  newLi.className = "list-group-item d-flex justify-content-between";
-  newLi.innerHTML = `<button class="btn btn-success mx-1 btn-sm" onclick="checkTask(this)">
-      <span class="material-symbols-outlined">Check</span>
-      </button>
-      <h4 class="flex-grow-1">${currentTask}</h4>
-      <button class="btn btn-warning mx-1" onclick="editTask(this)">
-        <span class="material-symbols-outlined"> edit </span>
-      </button>
-      <button class="btn btn-danger" onclick="removeTask(this)">
-        <span class="material-symbols-outlined"> delete </span>
-      </button>`;
-  parentList.appendChild(newLi);
-
-  currentInput.value = "";
-  saveToLocalStorage();
 }
 
 function removeTask(currElement) {
@@ -56,16 +66,14 @@ function removeTask(currElement) {
     newEmptyMsg.className = "d-flex justify-content-center";
     newEmptyMsg.classList.add("emptyMsg");
     parentList.appendChild(newEmptyMsg);
-    //   console.log(parentList.children[0].className);
 
     saveToLocalStorage();
+  } else {
+    saveToLocalStorage(); // Update local storage after deleting a task
   }
 }
 
 function editTask(currElement) {
-  // console.log(currElement.children[0]);
-  // // console.log(currElement.textContent);
-  // console.log(currElement.children[0].innerHTML)
   if (currElement.children[0].innerHTML == "done") {
     currElement.children[0].innerHTML = "edit";
     let currTask = currElement.previousElementSibling.value;
@@ -98,7 +106,6 @@ function editTask(currElement) {
 }
 
 function checkTask(currElement) {
-  console.log(currElement.parentElement);
   var currentLi = currElement.parentElement;
   currentLi.classList.toggle("changeColour");
   var h4 = currElement.parentNode.querySelector("h4");
